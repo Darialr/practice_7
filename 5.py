2627da615ec72a9e67e79d4397d13cca80a8b1f7
Darialr <darial.ryaguzova@gmail.com>
2024-04-03 18:41:36 +0700

    ready

--- 5.py
+++ 5.py
@@ -2,7 +2,7 @@ n = int(input())
 vol = 0
 length = 1
 while vol < n:
-    vol = length**3
+    vol = length ** 3
     if vol <= n:
         print(vol, end=' ')
     length += 1
---
+++ 6.py
@@ -0,0 +1,7 @@
+n = int(input())
+flwr = 1
+print(flwr)
+while flwr < n:
+    flwr *= 2
+    if flwr <= n:
+        print(flwr)
